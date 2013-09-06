f002405b2306d2099c9e34ac60bbc474ba1ff76a
Clearfield Christopher <chris.clearfield@gmail.com>
2013-09-06 07:34:06 -0400

    testing repository for js-routes

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :title, :author, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JsTest::Application.config.secret_key_base = '1caf595924bd62a6081a985c9f0ef91a028df104911708029b4b6a371fd7083f625ba8058b0306a6d00b5f9e4e4a6ffb1aefe0ed595518a8b653471697837f94'
